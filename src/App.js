e3c62ee93852f3fcbdb9c15ee320427eea2cb5ac
烟熏三文鱼 <1678803637@qq.com>
2021-07-14 20:50:10 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import { Upload, message, Button } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange (info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop (e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+}
+
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Dragger {...props}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+            band files
+          </p>
+        </Dragger>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button>Learn React</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
