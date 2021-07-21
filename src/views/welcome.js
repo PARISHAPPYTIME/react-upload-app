3b41cfae49606cf2f0c5b1284f12ae373e80238b
烟熏三文鱼 <1678803637@qq.com>
2021-07-21 08:46:59 +0800

    登录

--- src/views/welcome.js
+++ src/views/welcome.js
@@ -2,17 +2,34 @@ import { Input, Typography, Button, message } from 'antd'
 import React from 'react';
 import cookie from 'react-cookies'
 import { connect } from 'react-redux'
+import { UserOutlined, EyeInvisibleOutlined, MehOutlined } from '@ant-design/icons'
 
 const { Title } = Typography
 
 class WelcomePage extends React.Component {
 
     state = {
-        username: ''
+        username: '',
+        password: '',
+        repassword: ''
     }
 
-    handleChange = (e) => {
-        this.setName(e.target.value)
+    handleChangeUserName = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleChangePassWord = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    handleChangeRePassWord = (e) => {
+        this.setState({
+            repassword: e.target.value
+        })
     }
 
     setData = () => {
@@ -26,22 +43,24 @@ class WelcomePage extends React.Component {
         })
     }
 
-    handleUpload = () => {
-        cookie.save('username', this.state.username)
-        message.success(`欢迎 ${this.state.username} 使用软件`)
-        this.setData()
-        this.props.history.push({
-            pathname: '/main'
-        })
+    save = () => {
+        // cookie.save('username', this.state.username)
+        // message.success(`欢迎 ${this.state.username} 使用软件`)
+        // this.setData()
+        // this.props.history.push({
+        //     pathname: '/main'
+        // })
     }
 
     render() {
         return (
             <div className="common-width">
                 <Title>Introduction</Title>
-                <Input value={this.state.username} onChange={this.handleChange} size="large" placeholder="请输入你的名字" />
+                <Input prefix={<UserOutlined />} value={this.state.username} onChange={this.handleChangeUserName} size="large" placeholder="username" />
+                <Input prefix={<EyeInvisibleOutlined />} value={this.state.password} onChange={this.handleChangePassWord} size="large" placeholder="password" />
+                <Input prefix={<MehOutlined />} value={this.state.repassword} onChange={this.handleChangeRePassWord} size="large" placeholder="Enter your password again" />
                 <div className="w-btn">
-                    <Button onClick={this.handleUpload} type="primary" block>保存用户名称</Button>
+                    <Button onClick={this.save} type="primary" block>保存用户名称</Button>
                 </div>
             </div>
         )
